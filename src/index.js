c6ad9c7a4b26c4fa207d896cf1a11e7c8bd2f0d9
manasi-m <cyber.manasi@gmail.com>
2019-04-18 16:46:03 -0400

    add comments for overall rendering

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Imported components from the relative path to the file that holds them
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -5,5 +6,7 @@ import './App.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// 'root' is the overall component of the ReactDOM, which is the view that is rendered on the page
+// Here, we pass in the imported App component which comes from the App.js file
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
