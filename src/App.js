9dd0b6b35bfda0cddc98ac22bacf9b379e484869
manasi-m <cyber.manasi@gmail.com>
2019-04-18 16:46:45 -0400

    Add comments for functionality of weather app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+// Importing components from relative path 
 import React from "react";
 import Weather from "./components/weather";
 import Form from "./components/form";
 import Titles from "./components/titles";
 
+// API Key to be able to use the OpenWeatherMap API
 const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
 
 class App extends React.Component {
 
+  // Initialize state to undefined for all relevant data points 
   state = {
-
     temperature: undefined,
     city: undefined,
     country: undefined,
@@ -17,13 +19,18 @@ class App extends React.Component {
     error: undefined
   }
 
-  //getWeather is a method we'll use to make the api call
+  // getWeather is a method we'll use to make the API call
+  // It is passed in as the loadWeather prop to Form component in the render function below
+  // Use async keyword to specify it's an asynchronous function in response to an event e
   getWeather = async (e) => {
-
+    // Store city and country values based on current value in form
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     e.preventDefault();   
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    // fetch keyword for API call, await to show it's asynchronous, 
+    // URL defined at https://openweathermap.org/current
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${Api_Key}`);
+    // response stored as json in `response` variable
     const response = await api_call.json();
     console.log(response);
     if(city && country){
@@ -42,20 +49,26 @@ class App extends React.Component {
     }
   }
 
+  // Render function updates view whenever the state changes
+  // Components that were imported (Titles, Weather, Form) are called below as HTML tags, 
+  //   with props as attributes associated with that component
+  // Within the tag, props can be passed in to populate the component with the syntax
+  //   <componentName prop={value}>
+  // Look at the component definitions within the imported files (lines 2-5) 
+  //   to see how the props populate each component!
   render() {
 
     return (
-
       <div>
-         <div className="wrapper">
+        <div className="wrapper">
           <div className="main">
             <div className="container">
               <div className="row">
                 <div className="col-xs-5 title-container">
-                <Titles />
+                  <Titles />
                 </div>
                 <div className="col-xs-7 form-container">
-                <Form loadWeather={this.getWeather} />
+                  <Form loadWeather={this.getWeather} />
                   <Weather
                     temperature={this.state.temperature}
                     city={this.state.city}
@@ -74,4 +87,6 @@ class App extends React.Component {
     )
   }
 }
-export default App;
+
+// App is exported as a component which allows it to be imported by another file
+export default App
